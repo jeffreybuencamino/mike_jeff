b25b394a792b3487725aea403850bcb4649c302c
Your Name <your_email@example.com>
2025-07-30 21:57:07 -0700

    databases

--- web_application.py
+++ web_application.py
@@ -1,4 +1,17 @@
-from application import app
+from application import app, db
+from application.models import User, Post
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'db': db,
+        'User': User,
+        'Post': Post,
+        'sa': sa,
+        'so': so
+    }
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
